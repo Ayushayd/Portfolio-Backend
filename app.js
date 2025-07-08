631f7061ee0009cd61edcdb9bdd948faebb3843b
Ayush Gupta <ayushe19222@gmail.com>
2025-07-08 20:41:56 +0530

    added cron.js

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import fileUpload from 'express-fileupload';
-import dbConnection from './database/dbConnection.js';
-import {errorMiddleware} from './middlewares/error.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import dbConnection from "./database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
 import messageRouter from "./routes/messageRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import timelineRouter from "./routes/timelineRoutes.js";
 import applicationRouter from "./routes/softwareApplicationRoutes.js";
 import skillRoutes from "./routes/skillRoutes.js";
 import projectRoutes from "./routes/projectRoutes.js";
-
+import job from "./config/cron.js";
 
 const app = express();
 dotenv.config({
-    path: './config/config.env'
+  path: "./config/config.env",
 });
 
-app.use(cors({
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
+
+app.use(
+  cors({
     origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true
-})
+    credentials: true,
+  })
 );
 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
-}));
+  })
+);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+  });
+});
 
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
@@ -43,7 +56,5 @@ app.use("/api/v1/project", projectRoutes);
 
 dbConnection();
 app.use(errorMiddleware);
-
-
 
 export default app;
